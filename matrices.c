c03bd0f88ccace2a9876d3612080ed75f721a26c
Andres-Camacho-Bolanos <111325635+Andres-Camacho-Bolanos@users.noreply.github.com>
2022-11-22 14:59:04 -0600

    Update matrices.c

--- matrices.c
+++ matrices.c
@@ -1,1 +1,68 @@
+#include<stdio.h>
+#include <stdlib.h>
 
+#define F 10
+#define C 10
+
+      
+void suma_matrices(){
+    printf("Programa para calcular la suma de dos matrices\n\n");
+
+    int matriz_A[F][C] , matriz_B[F][C], matriz_C[F][C],n_filas, n_columnas,filas,columnas;
+    
+    printf("Matriz 1\n \n");
+
+    printf("Indique el número de filas:");scanf("%d",&n_filas);
+    printf("Indique el número de columnas:");scanf("%d",&n_columnas);
+
+    //Matriz a
+    
+    for (filas=0;filas < n_filas; filas ++){
+
+        for(columnas=0;columnas < n_columnas;columnas ++){
+            printf("Indique el elemento: [%d][%d]:",filas  + 1 ,columnas + 1);scanf("%d",&matriz_A[filas][columnas]);
+        }
+    }
+
+
+    //Matriz B
+    printf("\n");
+    printf("Matriz 2\n");
+    
+    printf("Indique el número de filas:");scanf("%d",&n_filas);
+    printf("Indique el número de columnas:");scanf("%d",&n_columnas);
+    printf("\n");
+
+    for(filas=0;filas < n_filas; filas ++){
+        for (columnas=0;columnas < n_columnas;columnas++){
+            printf("Indique el elemento:[%d][%d]:",filas  + 1 ,columnas + 1);scanf("%d",&matriz_B[filas][columnas]);
+        }
+    }
+
+          
+    //Suma de matrices
+    for(filas=0;filas < n_filas; filas ++){
+        for (columnas=0; columnas < n_columnas;columnas++){
+            
+            matriz_C [filas][columnas]= matriz_A[filas][columnas] + matriz_B[filas][columnas];
+    
+        }
+    }
+    
+    printf("\n");
+    //Impresión de la matriz
+    for (filas=0; filas < n_filas;filas ++){
+        
+        for(columnas=0;columnas < n_columnas;columnas ++){
+            printf("\t%d",matriz_C[filas][columnas]);
+        }
+        printf("\n");    
+    }
+    
+}
+
+
+int main(){
+
+    suma_matrices();
+}
