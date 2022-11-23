edbde563fb8b84200a5e5c29dfbe9b17238d314a
Andres-Camacho-Bolanos <111325635+Andres-Camacho-Bolanos@users.noreply.github.com>
2022-11-23 15:25:34 -0600

    Create menu.c

---
+++ menu.c
@@ -0,0 +1,24 @@
+//Programa para crear un menú con selección de múltiples programas//
+
+#include<stdio.h>
+#include<math.h>
+
+int main() {
+
+    int opción;
+ do
+ {
+    printf("\n 1.triángulo");
+    printf("\n 2.cuadrado");
+    pirntf("\n 3.factorial");
+    printf("\n 4.salario");
+    printf("\n 5.matrices");
+    printf("\n 6.matrices2");
+    printf("\n 7.matrices3");
+    printf("\n 8.Salir");
+//Se filtra la opción elegida//
+do
+{
+    printf(
+}
+} 
