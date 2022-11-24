6c56283371ad874c0f0c99b9e6f61aee1e367b37
Andres-Camacho-Bolanos <111325635+Andres-Camacho-Bolanos@users.noreply.github.com>
2022-11-23 23:16:52 -0600

    Update menu.c

--- menu.c
+++ menu.c
@@ -2,6 +2,62 @@
 
 #include<stdio.h>
 #include<math.h>
+//
+/* 
+Realiza un programa que calcule el area de la figura seleccionada:
+Cuadrado
+Trapecio
+Rectángulo
+*/
+void cuadrados(){
+
+    int opcion;
+    
+    printf("Menú:\n");
+    printf("\nElegir el número de la opcion deseada:\n\n1.Calcular el area de un cuadrado\n2.Calcular el area de un trapecio\n3.Calcular el area de un rectangulo ");
+    printf("\n\nOpción:");scanf("%i",&opcion); //SE LEE LA OPCIÓN SELECCIONADA//
+    int lado;
+    float b_mayor,b_menor,altura;
+    float base,r_altura;
+    float resultado;
+    
+	switch (opcion) //SE SELECCIONA EL CASO CORRESPONDIENTE A LAL OPCIÓN SELECCIONADA//
+        {
+            case 1:
+				printf("Valor del lado del cuadrado:");scanf("%i",&lado); //SE LEE EL VALOR DE UN LADO DEL CUADRADO//
+                resultado= lado * lado;
+                printf("El resultado es: %.2f",resultado); //SE IMPRIME EL ÁREA DEL CUADRADO//
+
+            break;
+
+            case 2:
+                
+                printf("Indicar el valor de la base mayor:");scanf("%f",&b_mayor);
+                printf("Indicar el valor de la base menor:");scanf("%f",&b_menor);
+                printf("Indicar el valor de la altura:");scanf("%f",&altura); //SE LEEN LAS PROPIEDADES DEL TRAPECIO//
+                resultado=((b_mayor + b_menor) * altura/2);
+                printf("El resultado es:%.2f",resultado); //SE IMPRIME EL ÁREA DEL TRAPECIO//
+            break;
+
+            case 3:
+               
+                printf("Indique el valor de la base y de la altura del rectangulo:");scanf("%f %f",&base,&r_altura); //SE LEEN LAS PROPIEDADES DEL RECTÁNGULO//
+                resultado= base * r_altura;
+                printf("El resultado es: %.2f",resultado); //SE IMPRIME EL ÁREA DEL RECTÁNGULO//
+            break;
+
+
+            default:
+                printf("No ha seleccionado una opción valida"); //SE IMPRIME UN MENSAJE PREDETERMINADO EN CASO DE NO SELECCIONAR UNA OPCIÓN VÁLIDA//
+        }
+
+}
+
+int main(){
+   cuadrados(); 
+
+    return 0;
+}
 //Programa para multiplicar matrices de 2x2//
 #define F 2
 #define C 2
@@ -371,7 +427,7 @@ switch (opción)
     case 2:
     printf("\n Iniciar programa cuadrado");
     scanf("%d", &n);
-    int cuadrado;
+    int cuadrados;
     break;
 
     case 3:
