7f1e5068ea31bd77caa2996c934d0a203dab8262
Andres-Camacho-Bolanos <111325635+Andres-Camacho-Bolanos@users.noreply.github.com>
2022-11-23 19:33:17 -0600

    Update menu.c

--- menu.c
+++ menu.c
@@ -47,6 +47,115 @@ void sueldo_neto(){
 	}
 	return 0;
 }
+//Programa para realizar conversiones de temperatura//
+void conversiones(){
+int opc, opc_c;
+float temperatura,resultado;
+
+
+void fahrenheit(){
+    
+    printf("Indique el valor de la temperatura:");scanf("%f",&temperatura); //SE LEE EL VALOR DE LA TEMPERATURA//
+    printf("\n\n");
+    printf("Indique la conversión deseada( seleccionar el número de la opción que corresponda): \n\n 1. F--> C \n 2. F-->K\n:");scanf("%i",&opc_c);
+    //SE LEE LA OPCIÓN SELECCIONADA//
+    
+    if (opc_c==1){
+        resultado= (temperatura - 32)*0.55 ;
+        printf("El resultado es %.2f °C",resultado);
+    
+    }
+    
+    else if(opc_c==2 ){
+        resultado= (temperatura + 459.67)*0.55 ;
+        printf("El resultado es %.2f K",resultado);
+    }    
+    
+
+    else{
+        printf("Por favor, seleccione una opción valida"); //SE IMPRIME EL RESULTADO DE LA OPCIÓN SELECCIONADA//
+    
+    }
+    
+}
+        
+
+void celcius(){
+    printf("Indique la conversión deseada (seleccionar el número de la opción que corresponda):\n1.C--> F \n2.C-->K\n:") ;scanf("%i",&opc_c);
+    printf("Indique el valor de la temperatura:");scanf("%f",&temperatura); //SE LEE EL VALOR DE LA TEMPERATURA//
+    
+    if (opc_c==1){
+        resultado= (temperatura * 1.8) + 32 ;
+        printf("El resultado es %.2f °F",resultado);
+    
+    }
+    
+    else if(opc_c==2 ){
+        resultado= (temperatura + 273.15) ;
+        printf("El resultado es %.2f K",resultado);
+    }    
+    
+
+    else{
+        printf("Por favor, seleccione una opción valida"); //SE IMPRIME EL RESULTADO DE LA OPCIÓN SELECCIONADA//
+    
+    }
+    
+}
+
+void kelvin(){
+
+    printf("Indique el valor de la temperatura:");scanf("%f",&temperatura); //SE LEE EL VALOR DE LA TEMPERATURA//
+    printf("Indique la conversión deseada: (seleccionar el número de la opción que corresponda)\n1.K--> C \n2.K-->F \n:");scanf("%i",&opc_c);
+     
+    if (opc_c==1){
+        resultado= (temperatura - 273.15);
+        printf("El resultado es %.2f °C",resultado);
+    
+    }
+    
+    else if(opc_c==2 ){
+        resultado= (temperatura - 273.15)*1.8 + 32 ;
+        printf("El resultado es %.2f °F",resultado);
+    }    
+    
+
+    else{
+        printf("Por favor, seleccione una opción valida"); //SE IMPRIME EL RESULTADO DE LA OPCIÓN SELECCIONADA//
+    
+    }
+}
+} 
+
+int main(){
+
+    printf("Indique la unidad del valor de la temperatura (seleccionar el número de la opción que corresponda):"); //SE LEE EL VALOR DE LA TEMPERATURA//
+    printf("\n");
+    printf("\n1.°F\n2.°C\n3. K\n:");scanf("%i",&opc);
+
+
+    switch(opc) //SE SELECCIONA LA OPCIÓN CORRESPONDIENTE//
+    { 
+        
+        case 1:
+        fahrenheit();
+        break;
+    
+        case 2:
+        celcius();
+        break;
+
+
+        case 3:
+        kelvin();
+        break;   
+    
+    
+        default:
+            printf("Por favor, seleccione una opción valida"); //SE IMPRIME UN MENSAJE PREDETERMINADO EN CASO DE NO SELECCIONAR UNA OPCIÓN VÁLIDA//
+    }    
+    return 0;
+}
 int main() {
 
     int n, opcion;
@@ -59,7 +168,8 @@ int main() {
     printf("\n 5.matrices");
     printf("\n 6.matrices2");
     printf("\n 7.matrices3");
-    printf("\n 8.Salir");
+    printf("\n 8.conversiones");
+    printf("\n 9.Salir");
 //Se filtra la opción que va a elegir el usuario//
 do
 {
@@ -111,7 +221,12 @@ switch (opción)
     scanf("%d", & n);
     int matrices 3;
     break;
+    
+    case 8:
+    printf("\n Iniciar programa conversiones");
+    scanf("%d" & n);
+    break;
 
-}while (opcion! =8);
+}while (opcion! =9);
 return 0;
 }
