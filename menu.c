9670303f30ee2d146900bf054835e809d433b62e
Andres-Camacho-Bolanos <111325635+Andres-Camacho-Bolanos@users.noreply.github.com>
2022-11-24 00:29:26 -0600

    Update menu.c

--- menu.c
+++ menu.c
@@ -2,6 +2,36 @@
 
 #include<stdio.h>
 #include<math.h>
+//Programa para generar una matriz transpuesta a partir de una original//
+void matriz_transpuesta(){	
+	int m, n, c, d, original[3][3], trans[3][3];
+	
+	printf("Ingrese el número de filas y columnas, separados por un espacio: ");
+	scanf("%d%d",&m,&n);
+	printf("Ingrese los elementos de la matriz: \n");
+	
+	for(c=0;c<m;c++){
+		for(d=0;d<n;d++){
+			scanf("%d",&original[c][d]);
+		}
+	}
+	
+	for(c=0;c<m;c++){
+		for(d=0;d<n;d++){
+			trans[d][c] = original[c][d];
+		}
+	}
+	
+	printf("La matriz transpuesta es: \n");
+	
+	for(c=0;c<n;c++){
+		for(d=0;d<m;d++){
+			printf("%d\t",trans[c][d]);
+		}  
+		printf("\n");
+	}
+	
+}
 //
 /* 
 Realiza un programa que calcule el area de la figura seleccionada:
