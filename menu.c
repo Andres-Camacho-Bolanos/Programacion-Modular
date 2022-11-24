9cea61eda1f559c46f2d987496f4d01406a938f9
Andres-Camacho-Bolanos <111325635+Andres-Camacho-Bolanos@users.noreply.github.com>
2022-11-23 19:23:51 -0600

    Update menu.c

--- menu.c
+++ menu.c
@@ -2,7 +2,34 @@
 
 #include<stdio.h>
 #include<math.h>
-
+//Programa para calcular el salario neto mensual//
+void sueldo_neto(){
+	float bono,iva_a,iva_r,salario_bruto,salario_neto;
+	 
+	printf("Introduzca el salario bruto:");scanf("%f",&salario_bruto); //SE INTRODUCE EL SALARIO BRUTO//
+	
+    printf("Introduzca el porcentaje de IVA Agregado:");scanf("%f",&iva_a);
+	iva_a = (iva_a/100)*salario_bruto;
+	
+    printf("Introduzca el porcentaje de IVA Retenido:");scanf("%f",&iva_r); //SE INTRODUCEN LOS VALORES DEL IVA//
+	iva_r = iva_r/100*salario_bruto;
+	salario_neto = (salario_bruto+iva_a)-iva_r;
+	
+    printf("El salario neto es:%.2f\n",salario_neto); //SE IMPRIME EL SALARIO NETO//
+	
+    if (salario_neto>10000) { //SI EL SALARIO NETO ES MENOR A $10,000, ENTONCES SE AGREGA UN BONO DEL %5//
+		printf("Introduzca el valor de su bono:");scanf("%f",&bono);
+		bono = (salario_neto*bono)/100;
+		salario_neto = salario_neto + bono;
+		
+        printf("El nuevo salario neto es:%.2f",salario_neto); //SE IMPRIME EL NUEVO SALARIO NETO//
+	} 
+    
+    else {
+		printf("Fin del programa");
+	}
+	return 0;
+}
 int main() {
 
     int n, opcion;
@@ -47,7 +74,7 @@ switch (opción)
     case 4:
     printf("\n Iniciar programa salario");
     scanf("%d", &n);
-    int salario;
+    int sueldo_neto;
     break;
 
     case 5:
@@ -70,4 +97,4 @@ switch (opción)
 
 }while (opcion! =8);
 return 0;
-} 
+}
